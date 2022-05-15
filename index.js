aec1f19c3815f1985101c6d6674bddefd236eae9
Darlley <systembrtt@gmail.com>
2022-05-15 14:30:40 -0400

    aula 11

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world")
+const fs = require('fs') // File System
+
+fs.readFile("arquivo1.txt","utf8", (err,data) => {
+  if(err){
+    console.log(err)
+    return
+  }
+  console.log(data)
+})
