e658f28334cf8da51bf0cd695a55faa7739ac845
Darlley <systembrtt@gmail.com>
2022-05-21 19:20:13 -0400

    aula 26

---
+++ index.js
@@ -0,0 +1,12 @@
+// mais de um valor
+
+const x = 10
+const y = "Algum texto"
+const z = [1,2]
+
+console.log(x,y,z)
+
+console.log(`o valor de x=${x}, de y=${y}, z=${z}`)
+console.log(`o valor de x=%s, de y=%s, z=%s`, x, y, z)
+
+console.clear()
