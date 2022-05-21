b01814dedb8477322f9929e88ea2208af919e481
Darlley <systembrtt@gmail.com>
2022-05-21 18:52:49 -0400

    aula 23

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const path = require('path')
-const extension = path.extname('arquivo.php')
+// none
 
-console.log(extension)
+console.log(process.argv)
+
+const args = process.argv.slice(2)
+
+console.log(args)
+
+const nome = args[0].split("=")[1]
+
+console.log(nome)
+
+const idade = args[1].split("=")[1]
+
+console.log(idade)
+
+console.log(`Nome dele é ${nome}, e tem ${idade} anos`)
+
+// node index.js nome=Darlley idade=30
