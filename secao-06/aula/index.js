786157a0e23e31c5e558563501a8e160f8bcd930
Darlley <systembrtt@gmail.com>
2025-02-06 21:20:40 -0400

    Middleware

--- secao-06/aula/index.js
+++ secao-06/aula/index.js
@@ -3,6 +3,19 @@ const app = express()
 
 const $_PORT = 3000
 
+const checkAuthMiddleware = function(req, res, next) {
+  req.authStatus = true
+  if(req.authStatus){
+    console.log('esta logado')
+    next()
+  } else {
+    console.log("Não esta logado, faça o login para continuar")
+    next()
+  }
+}
+
+app.use(checkAuthMiddleware)
+
 app.get('/', (req, res) => {
   res.send('Olá mundo!')
 })
