b8a78d66b3846292623b3bc65df56a83fee4f42b
Darlley <systembrtt@gmail.com>
2025-02-06 21:46:15 -0400

    Método POST

--- secao-06/aula/index.js
+++ secao-06/aula/index.js
@@ -2,12 +2,29 @@ const express = require('express')
 const path = require('node:path')
 const app = express()
 
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.use(express.json())
+
 const $_PORT = 3000
 const basePath = path.join(__dirname, 'templates')
+
+app.get('/users/add', (req, res) => {
+  res.sendFile(`${basePath}/userform.html`)
+})
 
 app.get('/users/:id', (req, res) => {
   const id = req.params.id
   console.log(`Estamos buscando pelo user ID: ${id}`)
+  res.sendFile(`${basePath}/users.html`)
+})
+
+
+app.post('/users/save', (req, res) => {
+  const { name, age } = req.body
+  console.log(`O usuário ${name}, ${age} anos, foi adicionado.`)
   res.sendFile(`${basePath}/users.html`)
 })
 
