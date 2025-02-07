2fdd3eaaf594f3575cafa6d7d4866b9bddc2114a
Darlley <systembrtt@gmail.com>
2025-02-06 20:27:54 -0400

    nodemon

--- secao-06/aula/index.js
+++ secao-06/aula/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const path = require('node:path')
 const app = express()
 
 const $_PORT = 3000
-const basePath = path.join(__dirname, 'templates')
 
 app.get('/', (req, res) => {
-  res.sendFile(`${basePath}/index.html`)
+  res.send('Olá mundo!')
 })
 
 app.listen($_PORT, () => {
