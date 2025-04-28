e5a0fce08fd7dac448fd11a6fc3b3304d639aad2
Darlley <systembrtt@gmail.com>
2025-04-28 15:50:18 -0400

    aula 112

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
-const app = express();
+
 const port = 3000;
-
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
 const auth = true
+
+const app = express();
+const hbs = exphbs.create({
+  partialsDir: ['views/partials']
+})
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
 
